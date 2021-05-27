6455257859c40af8ad06d6060cda120d32a45b56
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-27 16:20:20 -0500

    Finished application, uses giphy API to generate a random gif or a gif of the tag input.

--- src/components/Random_V2.jsx
+++ src/components/Random_V2.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import useGif from '../useGif';
 
 const Random = () => {
-    const { gif, fetchGif } = useGif(tag);
+    const { gif, fetchGif } = useGif();
 
     return (
         <div className="container">
-            <h1>Random {tag} Gif</h1>
+            <h1>Random Gif</h1>
             <img width="500" src={gif} alt="Random Gif" />
             <button onClick={fetchGif}>CLICK FOR NEW</button>
         </div>
